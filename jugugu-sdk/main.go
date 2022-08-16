5e29315e9660ff76c6b8610c8c4bbfc8cf7bb43b
qinchuan <1841068286@qq.com>
2022-08-16 19:48:02 +0800

    Update main.go

--- jugugu-sdk/main.go
+++ jugugu-sdk/main.go
@@ -24,27 +24,15 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
-	cfx, eth, err := bcc.RegByPrivateKey(bcc.TestIPandPort, bcc.ERC测试项目AAPID, "我擦1234567890", "8708ff69b5bb3383d3ae79996ce2ef81832b6570785644645cc356cb7a4e335d", "RegByPrivateKey")
+
+	// 注册账户
+	cfx, eth, err := bcc.Reg(bcc.TestIPandPort, bcc.ERC测试项目AAPID, "!!qinchuan@NFT", "Reg")
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
 		fmt.Println(cfx, eth)
 	}
-	// 多次批量创建NFT(-1)
-	// cfx, eth, err := bcc.Reg(bcc.TestIPandPort, bcc.ERC测试项目AAPID, "!!qinchuan@NFT", "Reg")
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// } else {
-	// 	fmt.Println(cfx, eth)
-	// }
 	// fmt.Println(acc.SHA256_strReturnString("123456"))
-	PrvKey, err := bcc.GetPrivateKey(bcc.TestIPandPort, bcc.ERC测试项目AAPID, "我擦1234567890", "cfxtest:aat0mupj008e71urzc72pgrnhdc5sv590a08r4ts8v", "GetPrivateKey")
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println(string(PrvKey))
-	}
-
 	// body, err := bcc.UserNFTURIPost(bcc.TestIPandPort, "CFX_TokenUri", bcc.ERC测试项目AAPID, "1", "CFX_TokenUri", "cfxs")
 	// if err != nil {
 	// 	fmt.Println(err.Error())
@@ -65,41 +53,42 @@ func main() {
 	// } else {
 	// 	fmt.Println(string(body))
 	// }
-	// //转移
-	// body, err = bcc.TransferFromPost(bcc.TestIPandPort, "CFX_TransferFrom", bcc.ERC测试项目AAPID, -1, 5000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, bcc.TestCFXAdministratorAddress, "6", "", "cfx")
-	// if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println(string(body))
-	// }
-	// body, err := bcc.AdminCreateNFTPost(bcc.TestIPandPort, "CFX_AdminCreateNFT", bcc.ERC测试项目AAPID, -1, 5000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, bcc.TestCFXAdministratorAddress, "", "cfx")
-	// if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println(string(body))
-	// }
+	//转移
+	body, err = bcc.TransferFromPost(bcc.TestIPandPort, "CFX_TransferFrom", bcc.ERC测试项目AAPID, -1, 5000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, bcc.TestCFXAdministratorAddress, "6", "", "cfx")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(body))
+	}
+	//管理员创建NFT
+	body, err := bcc.AdminCreateNFTPost(bcc.TestIPandPort, "CFX_AdminCreateNFT", bcc.ERC测试项目AAPID, -1, 5000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, bcc.TestCFXAdministratorAddress, "", "cfx")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(body))
+	}
 
 	// //批量转移
-	// var tos []string
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// tos = append(tos, bcc.TestCFXAdministratorAddress)
-	// var ids []string
-	// ids = append(ids, "469")
-	// ids = append(ids, "470")
-	// ids = append(ids, "471")
-	// ids = append(ids, "472")
-	// ids = append(ids, "473")
-	// ids = append(ids, "474")
-	// body, err := bcc.AdminTransferNFTBatchPost(bcc.TestIPandPort, "CFX_AdminTransferNFTBatch", bcc.ERC测试项目AAPID, -1, 50000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, tos, ids, "CFX_AdminTransferNFTBatch", "cfx")
-	// if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println(string(body))
-	// }
+	var tos []string
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	tos = append(tos, bcc.TestCFXAdministratorAddress)
+	var ids []string
+	ids = append(ids, "469")
+	ids = append(ids, "470")
+	ids = append(ids, "471")
+	ids = append(ids, "472")
+	ids = append(ids, "473")
+	ids = append(ids, "474")
+	body, err = bcc.AdminTransferNFTBatchPost(bcc.TestIPandPort, "CFX_AdminTransferNFTBatch", bcc.ERC测试项目AAPID, -1, 50000, bcc.TestAdministratorPassword, bcc.TestCFXAdministratorAddress, tos, ids, "CFX_AdminTransferNFTBatch", "cfx")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(body))
+	}
 }
 
 func 多次批量创建NFT(i int64) {
